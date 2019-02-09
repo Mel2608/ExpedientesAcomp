3d2ef83a357a0a6bda6269a567119941b487fec9
Mel Sol <mel.sol26@outlook.com>
2019-02-09 12:06:37 -0600

    Hoja Evolucion

--- ExpedientesAcomp/UI/HojaEvolucion.aspx
+++ ExpedientesAcomp/UI/HojaEvolucion.aspx
@@ -2,4 +2,51 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="ExpedientePrincipal.aspx">Expediente</a></li>
+            <li class="breadcrumb-item"><a href="ListaConsultas.aspx">Lista de Consultas</a></li>
+            <li class="breadcrumb-item"><a href="Consulta.aspx">Consulta</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Hoja Evolución</li>
+        </ol>
+    </nav>
+
+         <div class="container" aria-multiline="True">
+              <asp:Literal ID="lblMensaje" runat="server" Visible="false"></asp:Literal>
+             <br />
+                <div class="form-row">
+            <asp:Label ID="lblTitulo" runat="server" Text="Hoja de Evolución" Font-Bold="True" Font-Size="XX-Large" ForeColor="#16ACB8"></asp:Label>
+        </div>
+             <br />
+               <div>
+            <asp:Label ID="lblNombre" runat="server" Font-Bold="False" Text="Nombre Completo: "></asp:Label>
+        </div>
+         <div class="form-group ">
+                    <asp:TextBox type="text" class="form-control" ID="txtNombre" runat="server" BorderColor="#16ACB8" BorderStyle="Solid" onkeypress="if (event.keyCode == 13) { return false;}"></asp:TextBox>
+        </div>
+             <br />
+             <div>
+            <asp:Label ID="lblDiagnostico" runat="server" Font-Bold="False" Text="Diágnostico Médico: "></asp:Label>
+        </div>
+         <div class="form-group ">
+                    <asp:TextBox type="text"  class="form-control" ID="txtDiagnostico" runat="server" BorderColor="#16ACB8" BorderStyle="Solid" onkeypress="if (event.keyCode == 13) { return false;}"></asp:TextBox>
+        </div>
+             <br />
+              <div>
+            <asp:Label ID="lblEvolucion" runat="server" Font-Bold="False" Text="Evolución: "></asp:Label>
+        </div>
+              <div class="form-group ">
+                    <asp:TextBox type="text"  class="form-control" ID="txtEvolucion" runat="server" BorderColor="#16ACB8" BorderStyle="Solid" onkeypress="if (event.keyCode == 13) { return false;}" Rows="20" TextMode="MultiLine"></asp:TextBox>
+        </div>
+         
+                  <br />
+               <br />
+                <div class="form-group offset-sm-6">
+            <asp:Button ID="guardarBtn" type="button" class="btn btn-danger" runat="server" Text="Guardar"  ValidationGroup="guardar" />
+        </div>
+               <br />
+            
+
+
+             </div>
 </asp:Content>
--- ExpedientesAcomp/UI/HojaEvolucion.aspx.designer.cs
+++ ExpedientesAcomp/UI/HojaEvolucion.aspx.designer.cs
@@ -11,5 +11,86 @@ namespace UI {
     
     
     public partial class HojaEvaluacion {
+        
+        /// <summary>
+        /// Control lblMensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal lblMensaje;
+        
+        /// <summary>
+        /// Control lblTitulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTitulo;
+        
+        /// <summary>
+        /// Control lblNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombre;
+        
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+        
+        /// <summary>
+        /// Control lblDiagnostico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDiagnostico;
+        
+        /// <summary>
+        /// Control txtDiagnostico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDiagnostico;
+        
+        /// <summary>
+        /// Control lblEvolucion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblEvolucion;
+        
+        /// <summary>
+        /// Control txtEvolucion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEvolucion;
+        
+        /// <summary>
+        /// Control guardarBtn.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button guardarBtn;
     }
 }
