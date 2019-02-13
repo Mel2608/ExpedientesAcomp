8dd0cda513c4afd9c7d0ba6b803eebf0b1df20e8
Mel Sol <mel.sol26@outlook.com>
2019-02-12 22:28:01 -0600

    Psicología-UI

--- ExpedientesAcomp/UI/Psicologia.aspx
+++ ExpedientesAcomp/UI/Psicologia.aspx
@@ -1,4 +1,5 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PaginaMaestra.Master" AutoEventWireup="true" CodeBehind="Psicologia.aspx.cs" Inherits="UI.Psicologia" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
@@ -10,5 +11,86 @@
             <li class="breadcrumb-item active" aria-current="page">Psicología</li>
         </ol>
     </nav>
+
+    <div class="container" aria-multiline="True">
+        <asp:Literal ID="lblMensaje" runat="server" Visible="false"></asp:Literal>
+        <br />
+        <div class="form-row">
+            <asp:Label ID="lblTitulo" runat="server" Text="Psicología" Font-Bold="True" Font-Size="XX-Large" ForeColor="#16ACB8"></asp:Label>
+        </div>
+        <br />
+        <br />
+        <div>
+            <asp:Label ID="lblDiagnosMedico" runat="server" Font-Bold="False" Text="Diagnóstico Médico: "></asp:Label>
+        </div>
+        <div class="form-group ">
+            <asp:TextBox type="text" class="form-control" ID="txtDiagnMedico" runat="server" BorderColor="#16ACB8" BorderStyle="Solid" onkeypress="if (event.keyCode == 13) { return false;}"></asp:TextBox>
+        </div>
+        <br />
+        <div>
+            <asp:Label ID="lblTratamientos" runat="server" Font-Bold="False" Text="Tratamientos: "></asp:Label>
+        </div>
+        <div class="form-group ">
+            <asp:TextBox type="text" class="form-control" ID="txtTratamientos" runat="server" BorderColor="#16ACB8" BorderStyle="Solid" onkeypress="if (event.keyCode == 13) { return false;}" Rows="3" TextMode="MultiLine"></asp:TextBox>
+        </div>
+        <br />
+          <div>
+            <asp:Label ID="lblMotivoConsulta" runat="server" Font-Bold="False" Text="Motivo de consulta/ sintomatología: "></asp:Label>
+        </div>
+        <div class="form-group ">
+            <asp:TextBox type="text" class="form-control" ID="txtMotivoConsulta" runat="server" BorderColor="#16ACB8" BorderStyle="Solid" onkeypress="if (event.keyCode == 13) { return false;}" Rows="5" TextMode="MultiLine"></asp:TextBox>
+        </div>
+        <br />
+            <div>
+            <asp:Label ID="lblComposiciónFamiliar" runat="server" Font-Bold="False" Text="Composición familiar: "></asp:Label>
+        </div>
+        <div class="form-group ">
+            <asp:TextBox type="text" class="form-control" ID="txtComposicFam" runat="server" BorderColor="#16ACB8" BorderStyle="Solid" onkeypress="if (event.keyCode == 13) { return false;}" Rows="5" TextMode="MultiLine"></asp:TextBox>
+        </div>
+        <br />
+         <div>
+            <asp:Label ID="lblLaborEduc" runat="server" Font-Bold="False" Text="Aspectos Laborales y educativos: "></asp:Label>
+        </div>
+        <div class="form-group ">
+            <asp:TextBox type="text" class="form-control" ID="txtLaboralEducativ" runat="server" BorderColor="#16ACB8" BorderStyle="Solid" onkeypress="if (event.keyCode == 13) { return false;}" Rows="5" TextMode="MultiLine"></asp:TextBox>
+        </div>
+        <br />
+        <div>
+            <asp:Label ID="lblHistoria" runat="server" Font-Bold="False" Text="Historia Personal: "></asp:Label>
+        </div>
+        <div class="form-group ">
+            <asp:TextBox type="text" class="form-control" ID="txtHistPersonal" runat="server" BorderColor="#16ACB8" BorderStyle="Solid" onkeypress="if (event.keyCode == 13) { return false;}" Rows="8" TextMode="MultiLine"></asp:TextBox>
+        </div>
+        <br />
+         <div>
+            <asp:Label ID="lblDiagPresuntiv" runat="server" Font-Bold="False" Text="Impresión Diágnostica presuntiva/Psicodinamia: "></asp:Label>
+        </div>
+        <div class="form-group ">
+            <asp:TextBox type="text" class="form-control" ID="txtDiagPresuntiv" runat="server" BorderColor="#16ACB8" BorderStyle="Solid" onkeypress="if (event.keyCode == 13) { return false;}" Rows="5" TextMode="MultiLine"></asp:TextBox>
+        </div>
+        <br />
+          <div>
+            <asp:Label ID="lblObjsTerap" runat="server" Font-Bold="False" Text="Objetivos terapéuticos: "></asp:Label>
+        </div>
+        <div class="form-group ">
+            <asp:TextBox type="text" class="form-control" ID="txtObjsTerapeuticos" runat="server" BorderColor="#16ACB8" BorderStyle="Solid" onkeypress="if (event.keyCode == 13) { return false;}" Rows="5" TextMode="MultiLine"></asp:TextBox>
+        </div>
+         <br />
+          <div>
+            <asp:Label ID="lblTratamiento" runat="server" Font-Bold="False" Text="Tratamiento empleado: "></asp:Label>
+        </div>
+        <div class="form-group ">
+            <asp:TextBox type="text" class="form-control" ID="txtTratamiento" runat="server" BorderColor="#16ACB8" BorderStyle="Solid" onkeypress="if (event.keyCode == 13) { return false;}"></asp:TextBox>
+        </div>
+
+              <br />
+               <br />
+                <div class="form-group offset-sm-6">
+            <asp:Button ID="guardarBtn" type="button" class="btn btn-danger" runat="server" Text="Guardar"  ValidationGroup="guardar" />
+        </div>
+               <br />
+
+
+    </div>
 
 </asp:Content>
--- ExpedientesAcomp/UI/Psicologia.aspx.designer.cs
+++ ExpedientesAcomp/UI/Psicologia.aspx.designer.cs
@@ -11,5 +11,194 @@ namespace UI {
     
     
     public partial class Psicologia {
+        
+        /// <summary>
+        /// Control lblMensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal lblMensaje;
+        
+        /// <summary>
+        /// Control lblTitulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTitulo;
+        
+        /// <summary>
+        /// Control lblDiagnosMedico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDiagnosMedico;
+        
+        /// <summary>
+        /// Control txtDiagnMedico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDiagnMedico;
+        
+        /// <summary>
+        /// Control lblTratamientos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTratamientos;
+        
+        /// <summary>
+        /// Control txtTratamientos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTratamientos;
+        
+        /// <summary>
+        /// Control lblMotivoConsulta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMotivoConsulta;
+        
+        /// <summary>
+        /// Control txtMotivoConsulta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtMotivoConsulta;
+        
+        /// <summary>
+        /// Control lblComposiciónFamiliar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblComposiciónFamiliar;
+        
+        /// <summary>
+        /// Control txtComposicFam.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtComposicFam;
+        
+        /// <summary>
+        /// Control lblLaborEduc.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLaborEduc;
+        
+        /// <summary>
+        /// Control txtLaboralEducativ.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtLaboralEducativ;
+        
+        /// <summary>
+        /// Control lblHistoria.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblHistoria;
+        
+        /// <summary>
+        /// Control txtHistPersonal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtHistPersonal;
+        
+        /// <summary>
+        /// Control lblDiagPresuntiv.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDiagPresuntiv;
+        
+        /// <summary>
+        /// Control txtDiagPresuntiv.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDiagPresuntiv;
+        
+        /// <summary>
+        /// Control lblObjsTerap.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblObjsTerap;
+        
+        /// <summary>
+        /// Control txtObjsTerapeuticos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtObjsTerapeuticos;
+        
+        /// <summary>
+        /// Control lblTratamiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTratamiento;
+        
+        /// <summary>
+        /// Control txtTratamiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTratamiento;
+        
+        /// <summary>
+        /// Control guardarBtn.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button guardarBtn;
     }
 }
